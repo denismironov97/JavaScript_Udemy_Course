a69416ae112cb1e5c9e06850ca5cb8e978e02ac7
Denis M <denis.m.031997@gmail.com>
2023-10-11 16:55:51 +0300

    Solved Udemy challenge #2.

--- Tasks/udemy coding challenges/challenges.js
+++ Tasks/udemy coding challenges/challenges.js
@@ -29,4 +29,33 @@ function challengeOne() {
   }
 }
 
-challengeOne();
+function challengeTwo() {
+  //BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter);
+
+  let weightMark = 78;
+  let weightJohn = 92;
+
+  let heightMark = 1.69;
+  let heightJohn = 1.95;
+
+  const BMIMark = calculatePersonBMIFormulaOne(weightMark, heightMark);
+  const BMIJohn = calculatePersonBMIFormulaOne(weightJohn, heightJohn);
+
+  let stringResult;
+  if (BMIMark > BMIJohn) {
+    stringResult = "Mark's BMI is higher than John's!";
+  } else if (BMIMark < BMIJohn) {
+    stringResult = "John's BMI is higher than Mark's!";
+  } else {
+    stringResult = 'They both have the same BMI index.';
+  }
+
+  console.log(stringResult);
+
+  function calculatePersonBMIFormulaOne(personWeight, personHeight) {
+    const personBMI = personWeight / personHeight ** 2;
+    return personBMI;
+  }
+}
+
+challengeTwo();
