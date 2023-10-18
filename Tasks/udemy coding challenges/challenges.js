84b8a1626bfc188f6613f2983ea13e603360004f
Denis M <denis.m.031997@gmail.com>
2023-10-18 15:52:46 +0300

    Solved Udemy challenge #8.

--- Tasks/udemy coding challenges/challenges.js
+++ Tasks/udemy coding challenges/challenges.js
@@ -218,4 +218,54 @@ function challengeSeven() {
     }'s (${lowerBMIPerson.pBMI.toFixed(2)})!`;
   }
 }
-challengeSeven();
+//challengeSeven();
+
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+function challengeEight() {
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const totals = [];
+
+  const tipsArr = bills.map((bill) => {
+    const tipValue = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+    const currTotalValue = bill + tipValue;
+    totals.push(currTotalValue);
+
+    return tipValue;
+  });
+
+  console.log(`Bills: ${bills.join(', ')}`);
+  console.log(`Tips: ${tipsArr.join(', ')}`);
+  console.log(`Totals: ${totals.join(', ')}`);
+  console.log(`Average price is ${calcAverage(totals).toFixed(2)}`);
+
+  function calcAverage(arrArg) {
+    let sum = 0;
+    let index = 0;
+    while (index < arrArg.length) {
+      sum += arrArg[index];
+      index++;
+    }
+
+    return sum / arrArg.length;
+  }
+}
+challengeEight();
+
+function challengeEightV1() {
+  const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  };
+
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  const tips = [];
+  const totals = [];
+
+  for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+  }
+
+  console.log(bills, tips, totals);
+}
