4e03b9bd10b1efaedf62712b851913f2434d5b2a
Denis M <denis.m.031997@gmail.com>
2023-11-22 12:24:20 +0200

    Added notes on sets.

--- Sections/09-Data-Structures-Operators/starter/script.js
+++ Sections/09-Data-Structures-Operators/starter/script.js
@@ -314,6 +314,7 @@ for (const [i, el] of menu.entries()) {
 // console.log([...menu.entries()]);
 */
 
+/*
 ///////////////////////////////////////
 // ES6 enhanced object literals
 const catBodyParts = {
@@ -370,3 +371,40 @@ console.log(users[0]?.name ?? 'User array empty');
 
 if (users.length > 0) console.log(users[0].name);
 else console.log('user array empty');
+*/
+
+///////////////////////////////////////
+// Sets
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('jonasschmedtmann').size);
