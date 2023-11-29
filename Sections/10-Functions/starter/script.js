ba730375ea551cee90e7395a77779b719d314203
Denis M <denis.m.031997@gmail.com>
2023-11-29 17:22:55 +0200

    Completed mini closure challenge.

--- Sections/10-Functions/starter/script.js
+++ Sections/10-Functions/starter/script.js
@@ -239,3 +239,12 @@ const addVAT = addTax.bind(null, 0.23);
 
 console.log(addVAT(100));
 console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addTaxBrazil = addTaxRate(0.23);
+console.log(addTaxBrazil(200));
