ffea8dff7984564d4f7b811390b85e361dc3251d
Denis M <denis.m.031997@gmail.com>
2023-12-06 16:11:18 +0200

    Added notes on chaining methods.

--- Sections/11-Arrays-Bankist/lecture.js
+++ Sections/11-Arrays-Bankist/lecture.js
@@ -113,3 +113,19 @@ const max = movements.reduce((acc, mov) => {
   else return mov;
 }, movements[0]);
 console.log(max);
+
+///////////////////////////////////////
+// The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    // console.log(arr);
+    return mov * eurToUsd;
+  })
+  // .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
