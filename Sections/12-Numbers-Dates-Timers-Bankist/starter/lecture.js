ab554e53c74f5ec73149c0bb3556b35ae04ab25b
Denis M <denis.m.031997@gmail.com>
2023-12-18 17:32:24 +0200

    Added notes on Internationalizing numbers.

--- Sections/12-Numbers-Dates-Timers-Bankist/starter/lecture.js
+++ Sections/12-Numbers-Dates-Timers-Bankist/starter/lecture.js
@@ -6,6 +6,7 @@ for (let k = 0; k < 3; k++) {
 }
 */
 
+/*
 // Converting and Checking Numbers
 console.log(23 === 23.0);
 
@@ -220,3 +221,23 @@ const calcDaysPassed = (date1, date2) =>
 
 const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
+*/
+
+///////////////////////////////////////
+// Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
