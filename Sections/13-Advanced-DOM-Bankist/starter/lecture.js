54e002bb9725882a4e7d0375735bdef650d39370
Denis M <denis.m.031997@gmail.com>
2024-01-12 16:59:14 +0200

    Added notes on types of events and event handlers.

--- Sections/13-Advanced-DOM-Bankist/starter/lecture.js
+++ Sections/13-Advanced-DOM-Bankist/starter/lecture.js
@@ -89,3 +89,19 @@ logo.classList.contains('c'); // not includes
 // Don't use
 logo.clasName = 'jonas';
 */
+
+///////////////////////////////////////
+// Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
