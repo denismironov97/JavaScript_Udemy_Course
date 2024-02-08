aa31c5a31dbb3ab078a8ff930bc8adefc56e3198
Denis M <denis.m.031997@gmail.com>
2024-02-08 14:20:28 +0200

    Solved exercise #1

---
+++ Sections/14-OOP/starter/exercises.js
@@ -0,0 +1,44 @@
+//Exercise #1
+
+/*
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+*/
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+
+  return this.toString();
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+
+  return this.toString();
+};
+
+Car.prototype.toString = function () {
+  return `${this.speed}km/h-${this.make}`;
+};
+
+const car1 = new Car('Jaguar', 250);
+console.log(car1.accelerate());
+console.log(car1.brake());
+
+const car2 = new Car('BMW', 120);
+console.log(car2.brake());
+console.log(car2.accelerate());
+
+const car3 = new Car('Mercedes', 95);
+console.log(car3.brake());
+console.log(car3.accelerate());
