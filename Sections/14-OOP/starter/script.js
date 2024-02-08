90face8c3bdefcaafd510f7ae0f8c49e4734a53c
Denis M <denis.m.031997@gmail.com>
2024-02-08 14:19:58 +0200

    Added notes on prototypal inheritance on built-in objects

--- Sections/14-OOP/starter/script.js
+++ Sections/14-OOP/starter/script.js
@@ -14,7 +14,7 @@ const Person = function (firstName, birthYear) {
 };
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+//console.log(jonas);
 
 // 1. New {} is created
 // 2. function is called, this = {}
@@ -24,37 +24,76 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 
-console.log(jonas instanceof Person);
+//console.log(jonas instanceof Person);
 
+/*
 Person.hey = function () {
   console.log('Hey there 👋');
   console.log(this);
   console.log(`this context: ${this}`);
 };
 Person.hey();
+*/
 
 ///////////////////////////////////////
 // Prototypes
-console.log(Person.prototype);
+//console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 
-jonas.calcAge();
-matilda.calcAge();
+//jonas.calcAge();
+//matilda.calcAge();
 
+/*
 console.log(jonas.__proto__);
 console.log(jonas.__proto__ === Person.prototype);
 
 console.log(Person.prototype.isPrototypeOf(jonas));
 console.log(Person.prototype.isPrototypeOf(matilda));
 console.log(Person.prototype.isPrototypeOf(Person));
+*/
 
 // .prototyeOfLinkedObjects
-
+/*
 Person.prototype.species = 'Homo Sapiens';
 console.log(jonas.species, matilda.species);
 
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
+*/
+
+//console.log(Person.__proto__);
+//console.log(jack.__proto__.__proto__); //[Object: null prototype] {}
+
+//console.log(jonas.__proto__.__proto__);
+//console.log(jonas.__proto__ === Person.prototype);
+
+const arr1 = [1, 2, 3, 4, 5];
+console.log(arr1.__proto__);
+console.log(arr1.__proto__.__proto__);
+
+///////////////////////////////////////
+// Prototypal Inheritance on Built-In Objects
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
