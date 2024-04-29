e7649088d421c953ad7173f51dc41e9ec989cd72
Denis M <denis.m.031997@gmail.com>
2024-04-29 13:55:48 +0300

    ES6 Export/Import Modules Training - Renaming importing variables

--- Sections/17-Modern-JS-Modules-Tooling/starter/mainScriptModule.js
+++ Sections/17-Modern-JS-Modules-Tooling/starter/mainScriptModule.js
@@ -1,4 +1,10 @@
-import { stringVariable, cart, addToCart, tq } from './shoppingCartModule.js';
+import {
+  stringVariable,
+  cart,
+  addToCart,
+  tq,
+  totalPrice as totalCost,
+} from './shoppingCartModule.js';
 
 console.log('hello world!');
 
@@ -7,3 +13,5 @@ console.log(stringVariable);
 addToCart('pizza', 3);
 
 console.log(tq);
+
+console.log(totalCost);
