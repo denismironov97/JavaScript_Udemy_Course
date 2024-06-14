2d40d0982e7c1e3b77ef3bd847f12bd173005cef
Denis M <denis.m.031997@gmail.com>
2024-06-14 12:17:30 +0300

    Loading a recipe data from Forktify API.

--- Sections/18-forkify/starter/src/js/controller.js
+++ Sections/18-forkify/starter/src/js/controller.js
@@ -11,3 +11,45 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 */
+
+const getRecipeDataById = async function (recipeId) {
+  try {
+    const initialRes = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
+    );
+
+    if (!initialRes.ok) {
+      throw new Error(
+        `Initial server response failed -> status code: ${initialRes.status} status text: ${initialRes.statusText}!`
+      );
+    }
+
+    const {
+      data: { recipe },
+    } = await initialRes.json();
+
+    const regExPattern = /_([a-z])/g;
+    const replacementString = function (_, letter) {
+      return letter.toUpperCase();
+    };
+
+    const convertToCamelCase = str => {
+      return str.replace(regExPattern, replacementString);
+    };
+
+    const dataRecipe = Object.entries(recipe).reduce(
+      (acc, [currKey, value]) => {
+        const newKey = convertToCamelCase(currKey);
+        acc[newKey] = value;
+        return acc;
+      },
+      {}
+    );
+
+    console.log(dataRecipe);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+getRecipeDataById('5ed6604591c37cdc054bc886');
