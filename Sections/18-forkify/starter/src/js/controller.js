27729e62eb58e05939223da48c5ea60056b65e1b
Denis M <denis.m.031997@gmail.com>
2024-06-20 12:59:58 +0300

    Added construct recipe func.

--- Sections/18-forkify/starter/src/js/controller.js
+++ Sections/18-forkify/starter/src/js/controller.js
@@ -1,3 +1,8 @@
+'use strict';
+
+//Importing svg-s from source dir to use them in distribution dir
+import iconsSVG from 'url:../img/icons.svg';
+
 const recipeContainer = document.querySelector('.recipe');
 
 /*
@@ -37,7 +42,7 @@ const getRecipeDataById = async function (recipeId) {
       return str.replace(regExPattern, replacementString);
     };
 
-    const dataRecipe = Object.entries(recipe).reduce(
+    const recipeData = Object.entries(recipe).reduce(
       (acc, [currKey, value]) => {
         const newKey = convertToCamelCase(currKey);
         acc[newKey] = value;
@@ -46,10 +51,116 @@ const getRecipeDataById = async function (recipeId) {
       {}
     );
 
-    console.log(dataRecipe);
+    console.log(recipeData);
+
+    constructRecipe(recipeData);
   } catch (error) {
     console.log(error.message);
   }
 };
 
-getRecipeDataById('5ed6604591c37cdc054bc886');
+const constructRecipe = function ({
+  publisher,
+  ingredients,
+  sourceUrl,
+  imageUrl,
+  title,
+  servings,
+  cookingTime,
+  id,
+}) {
+  const recipeMarkup = `
+  <figure class="recipe__fig">
+    <img src="${imageUrl}" alt="${title}" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${title}</span>
+    </h1>
+  </figure>
+  
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${iconsSVG}.svg#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${cookingTime}</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${iconsSVG}.svg#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${servings}</span>
+      <span class="recipe__info-text">servings</span>
+  
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${iconsSVG}.svg#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${iconsSVG}.svg#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+  
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${iconsSVG}.svg#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${iconsSVG}.svg#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+  
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+      ${ingredients
+        .map(({ quantity, unit, description }) => {
+          return `
+        <li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${iconsSVG}.svg#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${quantity}</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${unit}</span>
+          ${description}
+        </div>
+      </li>
+        `;
+        })
+        .join('')}
+    </ul>
+  </div>
+  
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${publisher}</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${sourceUrl}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="${iconsSVG}.svg#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>
+  `;
+
+  recipeContainer.insertAdjacentHTML('afterbegin', recipeMarkup);
+};
+
+//getRecipeDataById('5ed6604591c37cdc054bc886');
