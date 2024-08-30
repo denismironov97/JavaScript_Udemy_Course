b5f8b0b4c4d0f39478a14b53395e1264697d3725
Denis M <denis.m.031997@gmail.com>
2024-08-30 12:17:43 +0300

    Added API key to make auth requests.

--- Sections/18-forkify/starter/src/js/config.js
+++ Sections/18-forkify/starter/src/js/config.js
@@ -2,10 +2,19 @@
 
 const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
+// API Key to make authorized requests or requests that change data or mutate in some sort of way on the backend database.
+const API_KEY = '1550d749-f40d-41d7-a7cd-d36f9979f9e3';
+
 const TIMEOUT_SECONDS = 10;
 
 const RESULTS_PER_PAGE = 10;
 
 const PAGINATION_LOAD_DELAY = 500;
 
-export { API_URL, TIMEOUT_SECONDS, RESULTS_PER_PAGE, PAGINATION_LOAD_DELAY };
+export {
+  API_URL,
+  TIMEOUT_SECONDS,
+  RESULTS_PER_PAGE,
+  PAGINATION_LOAD_DELAY,
+  API_KEY,
+};
